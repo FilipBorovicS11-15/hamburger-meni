09c7972bef9344f6a59e6c1adeda68b82f5ee981
FilipBorovicS11-15 <72797016+FilipBorovicS11-15@users.noreply.github.com>
2020-10-27 21:05:01 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+  $(".header-right > a").click(function() {
+    $(".hamburger-menu").addClass("active");
+  });
+
+  $(".close").click(function() {
+    $(".hamburger-menu").removeClass("active");
+  });
+
+});
